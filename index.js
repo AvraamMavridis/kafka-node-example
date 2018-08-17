709e227b9f0026148585389208d6b91d8dcc512d
AvraamMavridis <avraammauridis@gmail.com>
2018-08-17 16:30:23 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const Producer = require('./Producer');
+
+const topic = 'a_topic'
+
+const producer = new Producer();
+
+producer.init()
+  .then(() => producer.createTopics([topic]))
+  .then(() => producer.send(topic, ['I am a messasge']))
+  .catch(console.error)
